82980ae3b465188e94a0858187f02f6cc2bdc5d1
sanjobnth <sanjobnth@yahoo.in>
2016-08-05 21:12:54 +0530

    Data is fetched from The MovieDB API using AsyncTask

--- app/src/main/java/net/site400/sanjobnth/popularmovies/MainActivityFragment.java
+++ app/src/main/java/net/site400/sanjobnth/popularmovies/MainActivityFragment.java
@@ -1,9 +1,12 @@
 package net.site400.sanjobnth.popularmovies;
 
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 
 
@@ -60,8 +69,11 @@ public class MainActivityFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.action_refresh)
+        if(id == R.id.action_refresh) {
+            Log.v(LOG_TAG,"Refresh button clicked");
+            updatePopularMovies();
             return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -75,6 +87,81 @@ public class MainActivityFragment extends Fragment {
         GridView gridView = (GridView) rootView.findViewById(R.id.movies_grid);
         gridView.setAdapter(movieAdapter);
         return rootView;
+    }
+
+    private void updatePopularMovies(){
+        UpdatePopularMoviesTask updatePopularMoviesTask = new UpdatePopularMoviesTask();
+        updatePopularMoviesTask.execute();
+    }
+
+
+    public class UpdatePopularMoviesTask extends AsyncTask <Void, Void, Void> {
+        private final String LOG_TAG = UpdatePopularMoviesTask.class.getSimpleName();
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String popularMoviesJsonStr = null;
+
+
+            try {
+                Uri builtUri = Uri.parse("http://api.themoviedb.org/3/movie/popular?").buildUpon()
+                        .appendQueryParameter("api_key", "MY themoviedb API key")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+                Log.v(LOG_TAG, url.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                popularMoviesJsonStr = buffer.toString();
+                Log.v(LOG_TAG, popularMoviesJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                //e.printStackTrace();
+                return null;
+            }finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
     }
 
 }
