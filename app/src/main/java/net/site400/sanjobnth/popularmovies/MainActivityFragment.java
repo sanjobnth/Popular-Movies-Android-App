bc1963b722074168f31cb0d298543619e58f6b5a
sanjobnth <sanjobnth@yahoo.in>
2016-08-05 08:22:08 +0530

    Refresh Menu is added to MainActivityFragment

--- app/src/main/java/net/site400/sanjobnth/popularmovies/MainActivityFragment.java
+++ app/src/main/java/net/site400/sanjobnth/popularmovies/MainActivityFragment.java
@@ -2,8 +2,12 @@ package net.site400.sanjobnth.popularmovies;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
@@ -41,6 +45,25 @@ public class MainActivityFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_main_activity, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_refresh)
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
