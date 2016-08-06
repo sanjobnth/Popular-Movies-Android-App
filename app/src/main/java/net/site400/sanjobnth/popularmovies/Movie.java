61ebde30a312173dd6dfcdf695190d21e4f85505
sanjobnth <sanjobnth@yahoo.in>
2016-08-06 20:57:41 +0530

    feat: Adding other fields to Movie Class

--- app/src/main/java/net/site400/sanjobnth/popularmovies/Movie.java
+++ app/src/main/java/net/site400/sanjobnth/popularmovies/Movie.java
@@ -7,16 +7,27 @@ import android.os.Parcelable;
  * Created by SANJOY on 8/2/2016.
  */
 public class Movie implements Parcelable {
-    //int image;
-    String posterUrl;
 
-    public Movie(String posterUrl) {
-        //this.image = image;
+    String posterUrl;
+    String orgTitle;
+    String synopsis;
+    double rating;
+    String releaseDate;
+
+    public Movie(String posterUrl, String orgTitle, String synopsis, double rating, String releaseDate) {
         this.posterUrl = posterUrl;
+        this.orgTitle = orgTitle;
+        this.synopsis = synopsis;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
     }
 
     protected Movie(Parcel in) {
         posterUrl = in.readString();
+        orgTitle = in.readString();
+        synopsis = in.readString();
+        rating = in.readDouble();
+        releaseDate = in.readString();
     }
 
     public static final Creator<Movie> CREATOR = new Creator<Movie>() {
@@ -39,5 +50,9 @@ public class Movie implements Parcelable {
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         parcel.writeString(posterUrl);
+        parcel.writeString(orgTitle);
+        parcel.writeString(synopsis);
+        parcel.writeDouble(rating);
+        parcel.writeString(releaseDate);
     }
 }
